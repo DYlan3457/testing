11c3166365e47bfbc6d8471ed80b9e643c065e07
bob <gb1715rp@go.minneapolis.edu>
2015-05-12 14:49:23 -0500

    checkbox app

---
+++ Test Git Hub/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        MessageBox.Show("you checked the checkbox")
+    End Sub
+End Class
